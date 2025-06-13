157d871901ff9b89ad858eb569e59c7a4104065c
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-06-12 21:05:21 -0300

    feat: add contactCreateSchema for validating contact creation input

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -28,3 +28,27 @@ export const contactUpdateSchema = z.object({
     })
     .strict(),
 });
+
+export const contactCreateSchema = z.object({
+  body: z
+    .object({
+      FirstName: z.string().optional(),
+      LastName: z.string().optional(),
+      Salutation: z.string().optional(),
+      MailingStreet: z.string().optional(),
+      MailingCity: z.string().optional(),
+      MailingState: z.string().optional(),
+      MailingPostalCode: z.string().optional(),
+      MailingCountry: z.string().optional(),
+      Phone: z.string().optional(),
+      MobilePhone: z.string().optional(),
+      Email: z.string().email().optional(),
+      Title: z.string().optional(),
+      Department: z.string().optional(),
+      Level__c: z.string().optional(),
+      Languages__c: z.string().optional(),
+      Prequalified__c: z.boolean().optional(),
+      Loan_Amount__c: z.number().optional(),
+    })
+    .strict(),
+});
