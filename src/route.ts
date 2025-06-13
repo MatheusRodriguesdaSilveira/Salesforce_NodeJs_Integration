4bdff16994b567bb61dc847d4241081930d11cd6
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-06-12 21:04:55 -0300

    feat: update appRoutes to use 'route' parameter and add POST endpoint for contact creation

--- src/route.ts
+++ src/route.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify";
 import { GetContactsController } from "./controller/get-contacts.controller";
 import { UpdateContactController } from "./controller/update-contact.controller";
 import { GetContactIdController } from "./controller/get-contact-id.controller";
+import { CreateContactController } from "./controller/create-contact.controller";
 
-export async function appRoutes(fastify: FastifyInstance) {
-  fastify.get("/contacts", new GetContactsController().handle);
-  fastify.get("/contacts/:id", new GetContactIdController().handle);
-  fastify.put("/contacts/:id", new UpdateContactController().handle);
+export async function appRoutes(route: FastifyInstance) {
+  route.post("/contact", new CreateContactController().handle);
+  route.get("/contacts", new GetContactsController().handle);
+  route.get("/contacts/:id", new GetContactIdController().handle);
+  route.put("/contact/:id", new UpdateContactController().handle);
 }
